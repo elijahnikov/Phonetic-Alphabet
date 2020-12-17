080e746a8715679941a2eaf9aa6677e2b408b3c6
elijahnikov <posnikovelijah@gmail.com>
2020-12-17 15:50:28 +0000

    Adding first files

---
+++ App.xaml.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Phonetics
+{
+
+    public partial class App : Application
+    {
+
+        public void initGame()
+        {
+
+            ///((MainWindow)System.Windows.Application.Current.MainWindow).
+            ((MainWindow)System.Windows.Application.Current.MainWindow).entryBox.IsEnabled = true;
+            initTimer();
+            gameTimer.Start();
+            ((MainWindow)System.Windows.Application.Current.MainWindow).MainLetter.Content = getRandomChar();
+            letterDict = new Dictionary<string, string>
+            {
+                { "A", "alfa"},
+                { "B", "bravo"},
+                { "C", "charlie"},
+                { "D", "delta"},
+                { "E", "echo"},
+                { "F", "foxtrot"},
+                { "G", "golf"},
+                { "H", "hotel"},
+                { "I", "india"},
+                { "J", "juliett"},
+                { "K", "kilo"},
+                { "L", "lima"},
+                { "M", "mike"},
+                { "N", "november"},
+                { "O", "oscar"},
+                { "P", "papa"},
+                { "Q", "quebec"},
+                { "R", "romeo"},
+                { "S", "sierra"},
+                { "T", "tango"},
+                { "U", "uniform"},
+                { "V", "victor"},
+                { "W", "whiskey"},
+                { "X", "x-ray"},
+                { "Y", "yankee"},
+                { "Z", "zulu"}
+            };
+        }
+
+        public String getRandomChar()
+        {
+            String alphabetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            char[] alphabetArr = alphabetStr.ToCharArray();
+
+            Random rnd = new Random();
+
+            String letter = alphabetArr[rnd.Next(0, alphabetArr.Length)].ToString();
+
+            return letter;
+
+        }
+
+        public void initTimer()
+        {
+        
+            int msPerTurn = getSecondsPerTurn() * 1000;
+            gameTimer = new DispatcherTimer();
+            gameTimer.Tick += new EventHandler(mainTimer);
+            gameTimer.Interval = TimeSpan.FromMilliseconds(msPerTurn);
+
+        }
+
+        public void mainTimer(object source, EventArgs e)
+        {
+            ((MainWindow)System.Windows.Application.Current.MainWindow).MainLetter.Content = getRandomChar();
+        }
+
+
+        public void resetGame()
+        {
+            gameTimer.Stop();
+            currentTurn = 0;
+            numberIncorrect = 0;
+            numberCorrect = 0;
+            ((MainWindow)System.Windows.Application.Current.MainWindow).StartBtn.Visibility = Visibility.Visible;
+            ((MainWindow)System.Windows.Application.Current.MainWindow).PhoneticsLabel.Visibility = Visibility.Visible;
+            ((MainWindow)System.Windows.Application.Current.MainWindow).StopBtn.Visibility = Visibility.Hidden;
+            ((MainWindow)System.Windows.Application.Current.MainWindow).MainLetter.Visibility = Visibility.Hidden;
+            ((MainWindow)System.Windows.Application.Current.MainWindow).entryBox.IsEnabled = false;
+        }
+
+        public void mainLoop()
+        {
+
+            var entryField = ((MainWindow)System.Windows.Application.Current.MainWindow).entryBox;
+            string currentLetter = (string)((MainWindow)System.Windows.Application.Current.MainWindow).MainLetter.Content;
+
+            string letterKey = letterDict[currentLetter].ToLower();
+
+            if (!string.IsNullOrWhiteSpace(entryField.Text))
+            {
+                String userInput = entryField.Text.Trim().ToLower();
+                entryField.Text = String.Empty;
+                ((MainWindow)System.Windows.Application.Current.MainWindow).MainLetter.Content = getRandomChar();
+
+                //if correct
+                if (userInput == letterKey)
+                {
+                    Debug.WriteLine(true);
+                    //((MainWindow)System.Windows.Application.Current.MainWindow).MainLetter.Content = getRandomChar();
+
+                    gameTimer.Stop();
+                    gameTimer.Start();
+
+                    currentTurn++;
+                    numberCorrect++;
+
+                } 
+                //if incorrect                
+                else if (userInput != letterKey)
+                {
+                    Debug.WriteLine(false);
+
+                    gameTimer.Stop();
+                    gameTimer.Start();
+
+                    currentTurn++;
+                    numberIncorrect++;
+                }
+            }
+
+            if (currentTurn == getTurns()) 
+            {
+                Debug.WriteLine("Game has finished");
+                Debug.WriteLine("Number correct: " + numberCorrect);
+                Debug.WriteLine("Number incorrect: " + numberIncorrect);
+
+                ((MainWindow)System.Windows.Application.Current.MainWindow).correctLbl.Content = "Correct: " + numberCorrect;
+                ((MainWindow)System.Windows.Application.Current.MainWindow).incorrectLbl.Content = "Incorrect: " + numberIncorrect;
+                ((MainWindow)System.Windows.Application.Current.MainWindow).correctLbl.Visibility = Visibility.Visible;
+                ((MainWindow)System.Windows.Application.Current.MainWindow).incorrectLbl.Visibility = Visibility.Visible;
+
+                resetGame();
+            }
+        }
+
+        public void setTurns(int turns)
+        {
+            numberOfTurns = turns;
+        }
+
+        public void setSecondsPerTurn(int seconds)
+        {
+            secondsPerTurn = seconds;
+        }
+
+        public int getTurns()
+        {
+            return numberOfTurns;
+        }
+
+        public int getSecondsPerTurn()
+        {
+            return secondsPerTurn;
+        }
+
+        public DispatcherTimer gameTimer;
+        int secondsPerTurn = 4;
+        int numberOfTurns = 10;
+        int currentTurn = 0;
+        int numberCorrect = 0;
+        int numberIncorrect = 0;
+        public Dictionary<string, string> letterDict;
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace Phonetics
+{
+
+    public partial class MainWindow : Window
+    {   
+        //generic list of turn buttons
+        List<Button> turnList;
+        //generic list of seconds buttons
+        List<Button> secList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //add buttons to list
+            turnList = new List<Button>
+            {
+                numberOfTurns1, numberOfTurns2, numberOfTurns3
+            };
+
+            secList = new List<Button>
+            {
+                lengthPeriod1, lengthPeriod2, lengthPeriod3
+            };
+        }
+
+
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ((App)Application.Current).initGame();
+            entryBox.Focus();
+            PhoneticsLabel.Visibility = Visibility.Hidden;   
+            StartBtn.Visibility = Visibility.Hidden;
+            StopBtn.Visibility = Visibility.Visible;
+            MainLetter.Visibility = Visibility.Visible;
+            correctLbl.Visibility = Visibility.Hidden;
+            incorrectLbl.Visibility = Visibility.Hidden;
+        }
+
+        private void StopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ((App)Application.Current).resetGame();
+        }
+
+        private void SettingsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PhoneticsLabel.Visibility = Visibility.Hidden;
+            entryBox.Visibility = Visibility.Hidden;
+            entryRect.Visibility = Visibility.Hidden;
+            StartBtn.Visibility = Visibility.Hidden;
+
+            secondsLabel.Visibility = Visibility.Visible;
+            turnsLabel.Visibility = Visibility.Visible;
+
+            lengthPeriod1.Visibility = Visibility.Visible;
+            lengthPeriod2.Visibility = Visibility.Visible;
+            lengthPeriod3.Visibility = Visibility.Visible;
+
+            numberOfTurns1.Visibility = Visibility.Visible;
+            numberOfTurns2.Visibility = Visibility.Visible;
+            numberOfTurns3.Visibility = Visibility.Visible;
+
+            saveBtn.Visibility = Visibility.Visible;
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PhoneticsLabel.Visibility = Visibility.Visible;
+            entryBox.Visibility = Visibility.Visible;
+            entryRect.Visibility = Visibility.Visible;
+            StartBtn.Visibility = Visibility.Visible;
+
+            secondsLabel.Visibility = Visibility.Hidden;
+            turnsLabel.Visibility = Visibility.Hidden;
+
+            lengthPeriod1.Visibility = Visibility.Hidden;
+            lengthPeriod2.Visibility = Visibility.Hidden;
+            lengthPeriod3.Visibility = Visibility.Hidden;
+
+            numberOfTurns1.Visibility = Visibility.Hidden;
+            numberOfTurns2.Visibility = Visibility.Hidden;
+            numberOfTurns3.Visibility = Visibility.Hidden;
+
+            saveBtn.Visibility = Visibility.Hidden;
+        }
+
+        private void entryBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                ((App)Application.Current).mainLoop();
+            }
+        }
+
+        private void numberOfTurns1_Click(object sender, RoutedEventArgs e)
+        {
+            ((App)Application.Current).setTurns(10);
+            setColor(numberOfTurns1, 255, 255, 255);
+        }
+
+        private void numberOfTurns2_Click(object sender, RoutedEventArgs e)
+        {
+            ((App)Application.Current).setTurns(25);
+            setColor(numberOfTurns2, 255, 255, 255);
+        }
+
+        private void numberOfTurns3_Click(object sender, RoutedEventArgs e)
+        {
+            ((App)Application.Current).setTurns(50);
+            setColor(numberOfTurns3, 255, 255, 255);
+        }
+
+        private void lengthPeriod1_Click(object sender, RoutedEventArgs e)
+        {
+            ((App)Application.Current).setSecondsPerTurn(2);
+            setColor(lengthPeriod1, 255, 255, 255);
+        }
+
+        private void lengthPeriod2_Click(object sender, RoutedEventArgs e)
+        {
+            ((App)Application.Current).setSecondsPerTurn(4);
+            setColor(lengthPeriod2, 255, 255, 255);
+        }
+
+        private void lengthPeriod3_Click(object sender, RoutedEventArgs e)
+        {
+            ((App)Application.Current).setSecondsPerTurn(6);
+            setColor(lengthPeriod3, 255, 255, 255);
+        }
+
+        //set color of clicked button to white and set color of rest to default 
+        private void setColor(Button clickedBtn, byte r, byte g, byte b)
+        {
+
+            //if given button is lengthperiod button then change rest length period to default colours
+            //else if turn button then change rest of turn buttons to default colours
+            if (clickedBtn.Name.StartsWith("l")){
+                foreach (var button in secList)
+                {
+                    button.Background = new SolidColorBrush(Color.FromArgb(255, 220, 220, 220));
+                }
+            } else
+            {
+                foreach (var button in turnList)
+                {
+                    button.Background = new SolidColorBrush(Color.FromArgb(255, 220, 220, 220));
+                }
+            }
+            
+            clickedBtn.Background = new SolidColorBrush(Color.FromArgb(255, r, g, b));
+        }
+    }
+}
